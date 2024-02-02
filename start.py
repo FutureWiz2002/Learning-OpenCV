50b424d7be8431393f92a1660fbbbf33a076dfcc
Jabir Mustahid <jabirm2002@gmail.com>
2024-02-01 21:29:03 -0500

    Learned to use video to capture images

--- start.py
+++ start.py
@@ -1,10 +1,42 @@
-import cv2 as cv
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
-import sys
-img = cv.imread(cv.samples.findFile("macaw.jpg"))
-if img is None:
-    sys.exit("Could not read the image.")
-cv.imshow("Display window", img)
-k = cv.waitKey(0)
-if k == ord("s"):
-    cv.imwrite("macaw.jpg", img)
+# img = cv.imread('sunset-illustration-6000x4000-12659.jpg', cv.IMREAD_GRAYSCALE)
+# assert img is not None, "file could not be read, check with os.path.exists()"
+# edges = cv.Canny(img,100,200)
+# plt.subplot(121),plt.imshow(img,cmap = 'gray')
+# plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+# plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+# plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+# plt.show()
+
+
+cap = cv2.VideoCapture(0)
+    
+# while True: 
+    
+#     ret,img=cap.read()
+    
+#     cv2.imshow('Video', img)
+    
+#     if(cv2.waitKey(10) & 0xFF == ord('b')):
+#         break
+
+frameno = 0
+while(True):
+   ret,frame = cap.read()
+   if ret:
+      # if video is still left continue creating images
+      name = str(frameno) + '.jpg'
+      print ('new frame captured...' + name)
+
+      cv2.imwrite(name, frame)
+      frameno += 1
+   else:
+      break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
